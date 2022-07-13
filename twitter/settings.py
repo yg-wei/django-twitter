f4f471f9c0e957e59a846b0ccd0b35b8c9d939f3
Whipseryg <zhuoch86180@163.com>
2022-07-13 09:51:09 -0400

    Try django-debug-tool

--- twitter/settings.py
+++ twitter/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'p&z5uei2eljx8vl&=u283=6trlsu*99ixcljq*ffv7k+v30vbh'
 DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '192.168.33.10', 'localhost']
-
+INTERNAL_IPS = ['10.0.2.2']
 
 # Application definition
 
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
-    'accounts'
+    'accounts',
+    'debug_toolbar',
+
 ]
 
 REST_FRAMEWORK = {
@@ -55,6 +57,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'twitter.urls'
